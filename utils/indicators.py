d281b1a611d21b7f1f1c6c8b27f2b9ca66f17a7d
mukundvijay123 <115480143+mukundvijay123@users.noreply.github.com>
2024-07-12 20:38:26 +0530

    Update indicators.py

--- utils/indicators.py
+++ utils/indicators.py
@@ -1,15 +1,23 @@
 import pandas as pd
+from utils import profileManager as pm
+import os
 
-def dma(stockDataframe,length,index):
-    if(index>=length-1):
-        SumPrice=float(0)
-        for i in range(index -length,index):
-           
-            SumPrice=SumPrice+float(stockDataframe['Close'][i+1])
-        
-        return SumPrice/length
-    else:
-        return float(0)
+#Function for doing small updates
+#Column->column to derive dma from
+#days-> window for DMA
+def calcDma(df,column:str,days:int):
+    return df[column].rolling(window=days).mean()
 
-def dmaCalc(stockDataframe,length):
-    return stockDataframe.apply(lambda row : dma(stockDataframe,length,row.name),axis=1)
+
+#Change this function to only add dma if dma doenst exit already
+#days ->List of rolling window values
+def UpdateDfDma(ticker:str,column:str,days:list):
+    profile_dict=pm.get_profile()
+    derived_file_path=os.path.join(profile_dict["DerivedData"],f"{ticker[:-3]}.csv")
+    derived_his= pd.read_csv(derived_file_path)
+    for day in days:
+        derived_his[str(day)+"DMA"]=calcDma(derived_his,column,day)
+
+    derived_his.to_csv(derived_file_path,index=False)
+
+    
