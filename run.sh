9d70eb45bd7c3fa3a5146f03efd207c5d98dd867
mukundvijay123 <115480143+mukundvijay123@users.noreply.github.com>
2024-07-18 10:35:41 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 redis-server &
 REDIS_PID=$!
+
+source ./venv/bin/activate
+
+python engine.py 
+ENGINE_PID=$!
+
+ 
+
+cleanup(){
+	echo "Stopping all processes"
+	Kill ENGINE_PID REDIS_PID
+}
+
+trap cleanup EXIT
+wait $REDIS_PID $ENGINE_PID
